85a26e063f11294ae0dc982d6d9196da8e6965ce
wangwenguang <wangwenguang@dreame.tech>
2021-07-29 16:18:27 +0800

    create repo

---
+++ CMakeFiles/pcd_pic.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcd_pic.dir/main.cpp.o"
+  "CMakeFiles/pcd_pic.dir/originalVibe.cpp.o"
+  "pcd_pic.pdb"
+  "pcd_pic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcd_pic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
