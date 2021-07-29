dd025febe811c72cea4f6816d0c5d4724549b0ae
wangwenguang <wangwenguang@dreame.tech>
2021-07-29 16:24:47 +0800

    src code

---
+++ build/CMakeFiles/pcd_pic.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wangwengguang/dirty_mabu/opencv_vibe/main.cpp" "/home/wangwengguang/dirty_mabu/opencv_vibe/build/CMakeFiles/pcd_pic.dir/main.cpp.o"
+  "/home/wangwengguang/dirty_mabu/opencv_vibe/originalVibe.cpp" "/home/wangwengguang/dirty_mabu/opencv_vibe/build/CMakeFiles/pcd_pic.dir/originalVibe.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "qh_QHpointer"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/vtk-6.3"
+  "/usr/include/freetype2"
+  "/usr/include/x86_64-linux-gnu"
+  "/usr/local/include/pcl-1.11"
+  "/usr/local/include/eigen3"
+  "/usr/include/ni"
+  "/usr/include/openni2"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
